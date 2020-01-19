b720ba3a2a0978486d8947c5f40873b9ae7a1886
abc <abc@example.com>
2020-01-18 23:24:38 -0500

    OUra

--- Hackatown2020/ClientApp/src/app/home/home.component.ts
+++ Hackatown2020/ClientApp/src/app/home/home.component.ts
@@ -1,5 +1,22 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from
-  '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from'@angular/core';
+
+const heatMapData = [
+  {location: new google.maps.LatLng(37.782, -122.447), weight: 0.5},
+  new google.maps.LatLng(37.782, -122.445),
+  {location: new google.maps.LatLng(37.782, -122.443), weight: 2},
+  {location: new google.maps.LatLng(37.782, -122.441), weight: 3},
+  {location: new google.maps.LatLng(37.782, -122.439), weight: 2},
+  new google.maps.LatLng(37.782, -122.437),
+  {location: new google.maps.LatLng(37.782, -122.435), weight: 0.5},
+
+  {location: new google.maps.LatLng(37.785, -122.447), weight: 3},
+  {location: new google.maps.LatLng(37.785, -122.445), weight: 2},
+  new google.maps.LatLng(37.785, -122.443),
+  {location: new google.maps.LatLng(37.785, -122.441), weight: 0.5},
+  new google.maps.LatLng(37.785, -122.439),
+  {location: new google.maps.LatLng(37.785, -122.437), weight: 2},
+  {location: new google.maps.LatLng(37.785, -122.435), weight: 3}
+];
 
 @Component({
   selector: 'app-home',
@@ -10,19 +27,21 @@ export class HomeComponent implements AfterViewInit {
   @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
   map: google.maps.Map;
 
-  lat = 40.730610;
-  lng = -73.935242;
+  heatmap : any;
 
-  coordinates = new google.maps.LatLng(this.lat, this.lng);
+  lat = 45.641026;
+  lng = -73.499682;
+
+  montreal = new google.maps.LatLng(this.lat, this.lng);
 
   mapOptions: google.maps.MapOptions = {
-    center: this.coordinates,
-    zoom: 8,
+    center: this.montreal,
+    zoom: 13
   };
 
   marker = new google.maps.Marker({
-    position: this.coordinates,
-    map: this.map,
+    position: this.montreal,
+    map: this.map
   });
 
   ngAfterViewInit() {
@@ -30,9 +49,14 @@ export class HomeComponent implements AfterViewInit {
   }
 
   mapInitializer() {
-    this.map = new google.maps.Map(this.gmap.nativeElement,
-    this.mapOptions);
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
 
+    this.heatmap  = new google.maps.visualization.HeatmapLayer({
+      data: heatMapData
+    });
+
+    this.heatmap.setMap(this.map);
+  
     this.marker.setMap(this.map);
   }
 }

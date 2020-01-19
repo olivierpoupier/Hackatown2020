a3f2736aca687e6a4eb9aa6a69bbbcde6d6c0436
Olivier Poupier <olivier@maximiz.ca>
2020-01-18 23:42:11 -0500

    tamer

---
+++ Hackatown2020/BLL/AirPollutionCalculationLogic.cs
@@ -0,0 +1,25 @@
+﻿using Hackatown2020.Context;
+using Hackatown2020.DAL;
+using Hackatown2020.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hackatown2020.BLL
+{
+    public class AirPollutionCalculationLogic
+    {
+        private readonly StationsRepository _stationRepository;
+
+        public AirPollutionCalculationLogic(Hackatown2020Context ctx)
+        {
+            _stationRepository = new StationsRepository(ctx);
+        }
+        public async Task<CurrentDataDTO> getCurrentData()
+        {
+            // 
+
+        }
+    }
+}
--- Hackatown2020/Controllers/DataController.cs
+++ Hackatown2020/Controllers/DataController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hackatown2020.BLL;
+using Hackatown2020.Context;
+using Hackatown2020.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,12 +14,20 @@ namespace Hackatown2020.Controllers
     [Route("[controller]")]
     public class DataController : ControllerBase
     {
+        private readonly AirPollutionCalculationLogic _airPollutionCalculationLogic;
 
-        private readonly ILogger<DataController> _logger;
-
-        public DataController(ILogger<DataController> logger)
+        public DataController(Hackatown2020Context ctx)
         {
-            _logger = logger;
+            _airPollutionCalculationLogic = new AirPollutionCalculationLogic(ctx);
         }
+
+        [HttpGet]
+        public async Task<ActionResult<CurrentDataDTO>> getCurrentData()
+        {
+            CurrentDataDTO returnObject = await _airPollutionCalculationLogic.getCurrentData();
+
+            return Ok(returnObject);
+        }
+
     }
 }

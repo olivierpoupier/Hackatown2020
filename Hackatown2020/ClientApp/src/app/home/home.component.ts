0298c1b78137528c570866bd0b37234a6618fd64
Olivier Poupier <olivier@maximiz.ca>
2020-01-18 21:49:13 -0500

    suce ma kesh

--- Hackatown2020/ClientApp/src/app/home/home.component.ts
+++ Hackatown2020/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from
+  '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
+  map: google.maps.Map;
+
+  lat = 40.730610;
+  lng = -73.935242;
+
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 8,
+  };
+
+  marker = new google.maps.Marker({
+    position: this.coordinates,
+    map: this.map,
+  });
+
+  ngAfterViewInit() {
+    this.mapInitializer();
+  }
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement,
+    this.mapOptions);
+
+    this.marker.setMap(this.map);
+  }
 }
